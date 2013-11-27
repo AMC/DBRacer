0fb916dffc4075ee74d679df3c3ade28ab89bdc2
Andrew Canfield <Andrew@thePOG.net>
2013-11-26 23:59:26 -0800

    minor fix

--- notify.php
+++ notify.php
@@ -1,14 +1,16 @@
 <?
 
+  echo "checking for running processes";
+  $pid = `lsof -t -i:4444`
+
+  echo "attempting to kill $pid";
+  `kill -9 $pid`
+
   echo "pulling from origin master";
   
   `git pull origin master`
   
-  echo "checking for running processes";
-  $pid = `lsof -t -i:4444`
-  
-  echo "attempting to kill $pid";
-  `kill -9 $pid`
+
   
   `php -q server.php`
 
