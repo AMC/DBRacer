28da28da73b5843c12021248e1616fb94563f1a1
Andrew Canfield <Andrew@thePOG.net>
2013-11-29 21:22:43 -0800

    auto deploy fix

--- config.php
+++ config.php
@@ -1,4 +1,7 @@
 <?
 
-  $host       = $_SERVER['SERVER_NAME'];  //"ec2-54-211-53-228.compute-1.amazonaws.com"; 
-  $port       = '4444';
+  if ($_SERVER['SERVER_NAME'])
+    $host = $_SERVER['SERVER_NAME'];  
+  else
+    $host = "ec2-54-211-53-228.compute-1.amazonaws.com"; 
+  $port   = '4444';
