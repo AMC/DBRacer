1dc93de39336a741ed05dff8c2265606ab556838
Andrew Canfield <Andrew@thePOG.net>
2013-11-29 21:46:27 -0800

    aws changes domain names

--- config.php
+++ config.php
@@ -3,5 +3,5 @@
   if ($_SERVER['SERVER_NAME'])
     $host = $_SERVER['SERVER_NAME'];  
   else
-    $host = "ec2-54-211-53-228.compute-1.amazonaws.com"; 
+    $host = "ec2-54-211-13-122.compute-1.amazonaws.com"; 
   $port   = '4444';
