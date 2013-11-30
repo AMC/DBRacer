1a12a19db4eed9d4ddee70a14acca4dba70bf13e
Andrew Canfield <Andrew@thePOG.net>
2013-11-29 23:31:12 -0800

    php5.5 issues...

--- config.php
+++ config.php
@@ -1,7 +1,6 @@
 <?
 
-  if ($_SERVER['SERVER_NAME'])
-    $host = $_SERVER['SERVER_NAME'];  
-  else
-    $host = "ec2-54-211-13-122.compute-1.amazonaws.com"; 
+  $host = "ec2-54-211-13-122.compute-1.amazonaws.com"; 
   $port   = '4444';
+  
+?>
