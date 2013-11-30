52cd8e9fbd7cc36d561d4f340239466827f1d6c5
Andrew Canfield <Andrew@thePOG.net>
2013-11-30 01:05:42 -0800

    updated button text

--- js/eventHandlers.js
+++ js/eventHandlers.js
@@ -8,13 +8,19 @@ function connectionsHandler(data) {
 
   $(id).removeClass("btnStatus btnStatusConnected btnStatusReady btnStatusDropped");
   
-  if (data.message == "CONNECTED" || data.message == "NEW_CONNECTION")
+  if (data.message == "CONNECTED" || data.message == "NEW_CONNECTION") {
     $(id).addClass("btnStatusConnected");
+    $(id).val("Player " + data.id + "connected...");
+  }
     
-  if (data.message == "READY")
+  if (data.message == "READY") {
     $(id).addClass("btnStatusReady");
+    $(id).val("Player " + data.id + "ready...");
+  }
     
-  if (data.message == "CLOSED_CONNECTION")
+  if (data.message == "CLOSED_CONNECTION") {
     $(id).addClass("btnStatusDropped");
+    $(id).val("Player " + data.id + "dropped...");
+  }
 
 }
