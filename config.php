63b5e717bdd2301db2899f5425e6405bed02b49d
Andrew Canfield <Andrew@thePOG.net>
2013-12-03 10:22:43 -0800

    updated config.php

--- config.php
+++ config.php
@@ -1,4 +1,4 @@
 <?php
-  //$host = "ec2-54-211-13-122.compute-1.amazonaws.com"; 
-  $host = 'localhost';
+  $host = "ec2-54-211-13-122.compute-1.amazonaws.com"; 
+  //$host = 'localhost';
   $port = '4444';
