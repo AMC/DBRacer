ea636cefcd08e7c874d44c3e47645da45801641e
Andrew Canfield <Andrew@thePOG.net>
2013-12-03 10:41:00 -0800

    trying server alternative

--- config.php
+++ config.php
@@ -1,4 +1,6 @@
 <?php
-  $host = "ec2-54-211-13-122.compute-1.amazonaws.com"; 
-  //$host = 'localhost';
-  $port = '4444';
+  //$host = "ec2-54-211-13-122.compute-1.amazonaws.com"; 
+  $host = 'ec2-54-205-6-161.compute-1.amazonaws.com';
+  
+  //$port = '4444';
+  $port = '80';
