8ded6d1cbe529a789ef54151a705ea40db28a640
jischoler <jischoler@hotmail.com>
2013-12-03 15:50:58 -0800

    change update rate for draw

--- js/main.js
+++ js/main.js
@@ -237,7 +237,7 @@ function onTimer()
 	else if (start == 0)
 	{
 		document.getElementById("startLabel").innerHTML = "GO!";
-		mainloop = setInterval(getNewPosition, 20);
+		mainloop = setInterval(getNewPosition, 50);
 	}
 	else if (start == -2)
 	{
