a89e70938f201538532d5c40ca74dbe1fd3a7e9c
jischoler <jischoler@hotmail.com>
2013-12-04 11:24:14 -0800

    speed, laps changes

--- js/track.js
+++ js/track.js
@@ -4,10 +4,10 @@ function TrackData()
 	this.mapWidth = 32;
 	this.mapHeight = 16;
 	this.tileSize = 25;
-	this.maxSpeed = 3.0;
-	this.maxGrass = 2.0;
+	this.maxSpeed = 2.0;
+	this.maxGrass = 1.5;
 	this.speedRate = 0.1;
 	this.height = 400;
 	this.width = 800;
-	this.maxLaps = 2;
+	this.maxLaps = 3;
 }
