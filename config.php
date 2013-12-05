fc10a6b397b63d5563560152bf2f246ae397d69a
Andrew Canfield <Andrew@thePOG.net>
2013-12-05 15:59:22 -0800

    Trying to work around EWU firewall

--- config.php
+++ config.php
@@ -1,6 +1,7 @@
 <?php
   //$host = "ec2-54-211-13-122.compute-1.amazonaws.com"; 
-  $host = 'ec2-54-205-6-161.compute-1.amazonaws.com';
+  //$host = 'ec2-54-205-6-161.compute-1.amazonaws.com';
+  $host = 'dbracer.thepog.net';
   
   //$port = '4444';
-  $port = '80';
+  $port = '443';
