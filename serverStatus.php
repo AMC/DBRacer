a2422fe4e9350f7e226b7fa45031f9499f087d7e
Andrew Canfield <Andrew@thePOG.net>
2013-12-05 16:01:55 -0800

    update serverStatus to use config.php

--- serverStatus.php
+++ serverStatus.php
@@ -1,8 +1,9 @@
 <?php
+  require_once 'config.php';
 
   echo "checking for running processes...\n";
   echo "<br />";
-  $pid = `lsof -t -i:4444`;
+  $pid = `lsof -t -i:$port`;
   echo "process: $pid";
 
 
